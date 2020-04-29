ae056ac60fae81e791277772851347234b3cd995
Clarkey311 <jclarke89@outlook.com>
2020-04-29 17:04:56 +0100

    implimented basic Clarifi Colour Mode and passed through image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import FaceRecognition from './components/faceRecognition/FaceRecognition'
 import Particles from 'react-particles-js';
+import { render } from 'react-dom';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'd31ac431680f47478c760f80dfec5230'
+});
 
 const ParticleOptions = {
   particles: {
@@ -19,22 +26,52 @@ const ParticleOptions = {
 
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={ParticleOptions}
-        />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<Logo />
-      <ImageLinkForm />
-      <FaceRecognition />
-      */}
-    </div>
+class App extends Component {
+  constructor() {
+    super ();
+    this.state = {
+      input: '',
+      imageURL: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+    console.log(event.target.value);
+
+  }
+
+  onSubmit = () => {
+    this.setState({imageURL: this.state.input})
+    console.log('click');
+    app.models
+    .predict(
+    Clarifai.COLOR_MODEL, this.state.input
+    )
+    .then(function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
   );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={ParticleOptions}
+          />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageURL={this.state.imageURL}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
