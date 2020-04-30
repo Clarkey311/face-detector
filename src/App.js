dbddf07cc131675b0d5529a27256969ea3e3a230
Clarkey311 <jclarke89@outlook.com>
2020-04-30 17:56:15 +0100

    Created basic routing and registration form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
-import FaceRecognition from './components/faceRecognition/FaceRecognition'
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
+import SignIn from './components/signIn/SignIn';
+import Register from './components/register/Register';
 import Particles from 'react-particles-js';
 import { render } from 'react-dom';
 import Clarifai from 'clarifai';
@@ -32,34 +34,35 @@ class App extends Component {
     this.state = {
       input: '',
       imageURL: '',
-      box: {}
+      box: {},
+      route: 'signIn',
+      isSignedIn: false
     }
   }
 
-calculateFaceLocation = (data) => {
+  calculateFaceLocation = (data) => {
 
-  const clariaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-  const image = document.getElementById('inputImage');
-  const width = Number(image.width);
-  const height = Number(image.height);
-  console.log(width, height);
-  return {
-    leftCol: clariaiFace.left_col * width,
-    topRow: clariaiFace.top_row * height,
-    rightCol: width - (clariaiFace.right_col * width),
-    bottomRow: height - (clariaiFace.bottom_row * height)
+    const clariaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clariaiFace.left_col * width,
+      topRow: clariaiFace.top_row * height,
+      rightCol: width - (clariaiFace.right_col * width),
+      bottomRow: height - (clariaiFace.bottom_row * height)
+    }
+  };
+
+  displayBox = (box) => {
+    console.log('Box', box);
+    this.setState({box: box});
   }
-};
-
-displayBox = (box) => {
-  console.log('Box', box);
-  this.setState({box: box});
-}
 
   onInputChange = (event) => {
     this.setState({input: event.target.value})
     console.log(event.target.value);
-
   }
 
   onSubmit = () => {
@@ -73,21 +76,41 @@ displayBox = (box) => {
     .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    if(route === 'signOut'){
+      this.setState({isSignedIn: false})
+    }else if(route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageURL, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
           params={ParticleOptions}
-          />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
-        <FaceRecognition imageURL={this.state.imageURL} box={this.state.box}/>
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+        ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+            <FaceRecognition imageURL={imageURL} box={box}/>
+          </div>
+        :(
+          route === 'signIn'
+          ? <SignIn onRouteChange={this.onRouteChange} />
+          : <Register onRouteChange={this.onRouteChange} />
+          )
+        }     
       </div>
     );
   }
-
 }
+
+
 
 export default App;
